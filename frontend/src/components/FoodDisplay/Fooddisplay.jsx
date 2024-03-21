473c05f2a40d10ad67e62e6b672114d37d33aece
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-03-21 12:07:17 +0530

    fooddisplay

--- frontend/src/components/FoodDisplay/Fooddisplay.jsx
+++ frontend/src/components/FoodDisplay/Fooddisplay.jsx
@@ -1,25 +1,29 @@
 import React, { useContext } from "react";
 import "./Fooddisplay.css";
-import { food_list } from "./../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 import Fooditem from "../Fooditem/Fooditem";
+
 const Fooddisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+
   return (
     <div className="food-display" id="food-display">
       <h2>Top Dishes Near You</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return (
-            <Fooditem
-              key={index}
-              id={item._id}
-              name={item.name}
-              decription={item.decription}
-              price={item.price}
-              image={item.image}
-            />
-          );
+          if (category === "All" || category === item.category) {
+            return (
+              <Fooditem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+          return null; // If the condition is not met, return null
         })}
       </div>
     </div>
