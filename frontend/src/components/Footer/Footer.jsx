6c86658b6d4f5b319f009ff54822e9745ba6f00f
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-03-21 12:36:18 +0530

    footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non quam
+            hic assumenda possimus tempora.
+          </p>
+          <div className="footer-social-icon">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery </li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in Touch</h2>
+          <ul>
+            <li>+1-123-456-789</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 Toamato. All Rights Resrved
+      </p>
+    </div>
+  );
+};
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+export default Footer;
